7d698326b8a0f21c5a6e852975ca40336e8fd250
march-dave <ysangy@gmail.com>
2018-05-19 20:05:56 -0400

    code rollback ios emulator not running

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-// import App from './App';
-import App from './src/components/App';
+import App from './App';
+// import App from './src/components/App';
 
 AppRegistry.registerComponent('AngelHack2018', () => App);
